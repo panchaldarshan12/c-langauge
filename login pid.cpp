f78792b537b9f8362df4795a3eabf84a4a9c2c0c
panchaldarshan12 <121470465+panchaldarshan12@users.noreply.github.com>
2023-01-20 07:59:22 +0530

    Add files via upload

---
+++ login pid.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int main()
+
+{
+    printf("...................................................HR Department........................................................\n");  
+    int u1=177,p1=177;
+    int u3,p3;
+    printf("Enter Username:");
+    scanf("%d",&u3);
+    printf("Enter Password:");
+    scanf("%d",&p3);
+    if(u1==u3&&p1==p3)
+    {
+        printf("Login Successfully!!\n");
+    }
+    else
+    {
+        printf("Wrong Password or Username!!\n");
+    }
+    printf("Department Area - Motera \n");
+    printf("Your Joining Date : 28/09/2022\n");
+    printf("Your experience : 2 Years \n");
+    printf("Designation : Manager\n");
+    printf("Salary : Rs 50,000\n");
+    
+
+    
+{
+
+	char fname[7],lname[5],mname[10],age[1],dob[9],contact[9],mail[40];
+	printf("......................................................ID CARD...........................................................\n");
+	printf("Your First Name : ");
+	scanf("%s",fname);
+	printf("Your Last name : ");
+	scanf("%s", lname);
+	printf("Your Age :");
+	scanf("%s",age);
+	printf("Your Date Of Birth :");
+	scanf("%s",dob);
+	printf("Your Contact Number :");
+	scanf("%s", contact);
+	printf("Your Email ID :");
+	scanf("%s",mail);
+	}
+    return 0;
+}
+ 
+
